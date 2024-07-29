75a4d307f6e1849bf4608b023cb15a466bfe2c26
gngap-016 <gilangajipanutan16@gmail.com>
2024-07-30 00:01:54 +0700

    Link Typo

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,18 +10,18 @@ export const metadata = {
     title: 'Lamidi & Iba Lestari',
     description: 'Undangan Pernikahan Lamidi, S.Pd. dan Iba Lestari, A.Md.Keb.',
     // url: process.env.HOST_URL,
-    url: 'https://wedding-lamidi.vercel.app/',
+    url: 'https://wedding-lamidi-dan-iba-lestari.vercel.app/',
     siteName: 'Undangan Pernikahan Lamidi, S.Pd. dan Iba Lestari, A.Md.Keb.',
     images: [
       {
         // url: `${process.env.HOST_URL}images/couple1.jpg`,
-        url: `https://wedding-lamidi.vercel.app/images/couple1.jpg`,
+        url: `https://wedding-lamidi-dan-iba-lestari.vercel.app/images/couple1.jpg`,
         width: 800,
         height: 600,
       },
       {
         // url: `${process.env.HOST_URL}images/couple1.jpg`,
-        url: `https://wedding-lamidi.vercel.app/images/couple1.jpg`,
+        url: `https://wedding-lamidi-dan-iba-lestari.vercel.app/images/couple1.jpg`,
         width: 1800,
         height: 1600,
         alt: 'Undangan Pernikahan Lamidi & Iba Lestari',
